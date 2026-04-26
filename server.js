dfc4530ae5f0371ab8037bacd7763b4ad72902d8
Bohdan <SimBa5762@users.noreply.github.com>
2026-04-26 21:02:15 +0300

    added route for genres

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const moviesRouter = require('./routes/movies');
 const usersRouter = require('./routes/users');
-
+const genresRouter = require('./routes/genres');
 
 
 const app = express();
@@ -17,9 +17,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use('/api/movies', movieRouter);
-app.use('/api/users', userRouter);
-
+app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/genres', genresRouter);
 
 
 
